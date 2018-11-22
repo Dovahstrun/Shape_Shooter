91a0ff8b15ede051739287e0aaa6f9b8ddd4cf0f
Craig Fletcher <43002140+Dovahstrun@users.noreply.github.com>
2018-11-22 17:04:04 +0000

    Initial commit

---
+++ Game Code/Headers/Level.h
@@ -0,0 +1,27 @@
+#pragma once
+
+//Library Includes
+#include <SFML/Graphics.hpp>
+
+class Level
+{
+
+public:
+
+	Level();
+
+	virtual void Draw(sf::RenderTarget& _target);
+	virtual void Update(sf::Time _frameTime);
+
+	void loadLevel(int _levelToLoad);
+	void ReloadLevel();
+	int GetCurrentLevel();
+
+	int getCellSize();
+
+private:
+
+	const float m_cellSize;
+	int m_currentLevel;
+	std::vector<std::vector<sf::Sprite> > m_background;
+};
---
+++ Main.cpp
@@ -0,0 +1,120 @@
+// Library Includes
+#include <SFML/Window.hpp>	
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+
+// Project Includes
+#include "Framework/Headers/AssetManager.h"
+#include "Game Code/Headers/Level.h"
+
+// The main() Function - entry point for our program
+int main()
+{
+	///-------------------------------------------------
+	///SETUP
+	///-------------------------------------------------
+
+	// Window - to draw to the screen
+	sf::RenderWindow gameWindow;
+	gameWindow.create(sf::VideoMode::getDesktopMode(), "Sokoban", sf::Style::Titlebar | sf::Style::Close);
+
+	// Seed our random number generator 
+	srand(time(NULL));
+
+	// Create AssetManager
+	AssetManager assets;
+
+	// Game Clock - to keep track of time passed each frame
+	sf::Clock gameClock;
+
+	//Create the game level
+	Level ourLevel;
+
+	///-------------------------------------------------
+	///END SETUP
+	///-------------------------------------------------
+
+
+
+	///-------------------------------------------------
+	///GAME LOOP
+	///-------------------------------------------------
+
+	while (gameWindow.isOpen()) //Beginning of the game loop
+	{
+		///---------------------------------------------
+		///INPUT
+		///---------------------------------------------
+
+		// Check all events since the last frame and process each one until there are no more
+
+		sf::Event gameEvent; //A variable to store any event
+
+		while (gameWindow.pollEvent(gameEvent))
+		{
+			// TODO: Pass event to input objects
+
+
+			// Did the player try to close the window?
+			if (gameEvent.type == sf::Event::Closed)
+			{
+				// If so, call the close function on the window.
+				gameWindow.close();
+			}
+
+		} // End event polling loop
+
+		///---------------------------------------------
+		///END INPUT
+		///---------------------------------------------
+
+
+
+		///---------------------------------------------
+		///UPDATE
+		///---------------------------------------------
+
+		// Get the time passed since the last frame and restart our game clock
+		sf::Time frameTime = gameClock.restart();
+
+		//End the game when escape is pressed
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+		{
+			return 0;
+		}
+
+		//Pass update to level
+		ourLevel.Update(frameTime);
+
+		///---------------------------------------------
+		///END UPDATE
+		///---------------------------------------------
+
+
+
+		///---------------------------------------------
+		///DRAW
+		///---------------------------------------------
+
+		// Clear the window to a single colour
+		gameWindow.clear(sf::Color::Black);
+
+
+		//Pass draw to level
+		ourLevel.Draw(gameWindow);
+
+		// Display the window contents on the screen
+		gameWindow.display();
+
+		///---------------------------------------------
+		///END DRAW
+		///---------------------------------------------
+
+	} // End of Game Loop
+
+	return 0;
+
+} // End of main() Function
